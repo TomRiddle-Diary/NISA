a5554eb2bafc765e78306d5d6c4d7a739629e2d5
keiji <BB123021@sun.ac.jp>
2024-12-08 16:18:57 +0900

    my change

--- nisa_model.py
+++ nisa_model.py
@@ -1,5 +1,6 @@
 total = 0
 count = 0
+original = 0
 want = int(input("How much money you want?: "))
 monthly_invest = int(input("How much you invest per month? (万): ")) * 10000
 interest_rate = int(input("Interest Rate (%): ")) / 100
@@ -8,6 +9,7 @@ while total < want:
     total += monthly_invest
     total += total * (interest_rate / 12)
     count += 1
+    original += monthly_invest
 
 years = count / 12
 
@@ -22,7 +24,9 @@ while total < want:
 
 years = count / 12
 
-print(f"You will reach {want:,} yen in {years:.2f} years if you just saving.")
+print(f"You will reach {want:,} yen in {years:.2f} years if you just saving.\n")
+
+print(f"Your original money is {original:,}")
 
 
 
